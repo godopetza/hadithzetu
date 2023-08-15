f0413a61633e6bde326547e2238b7c3057a9cb96
Benard Mseti <andrewbenard7@gmail.com>
2023-08-15 18:41:02 +0300

    HadithiZETU v0.1.3 WIP

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "5ddf9b141409d2c0063b8a98585ddd8e",
-"index.html": "6bec67ceff4c56d0ed4bd759df169b35",
-"/": "6bec67ceff4c56d0ed4bd759df169b35",
-"main.dart.js": "db8e2981f88717eadde3f12d09d6a882",
+"index.html": "a1bdd85b94084091136ac2e57935d9ee",
+"/": "a1bdd85b94084091136ac2e57935d9ee",
+"main.dart.js": "a75f951082ac57bbedd3343c924b31f1",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "05963be36d04040658477a12427ba2de",
 "icons/Icon-192.png": "05963be36d04040658477a12427ba2de",
